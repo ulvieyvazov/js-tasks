059e0baa32deefb644448857d3adb9cdc1d6a46a
ulvieyvazov <eyvazovulvi010101@gmail.com>
2023-04-19 00:16:15 +0400

    add correction

---
+++ task2/app.js
@@ -0,0 +1,15 @@
+const test = (param) => {
+    let count = 0;
+    while (param !== 1) {
+        if (param % 2 === 0) {
+            param = param / 2
+        }
+        else{
+            param = param + 1
+        }
+
+        count++
+    }
+    return count;
+}
+console.log(test(23));
