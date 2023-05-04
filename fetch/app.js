2eb92c98050b0563d29623ff15c41454caf89aa1
ulvieyvazov <eyvazovulvi010101@gmail.com>
2023-05-04 18:13:34 +0400

    add cart api

--- fetch/app.js
+++ fetch/app.js
@@ -3,29 +3,37 @@ const container = document.querySelector(".container")
 const header = document.querySelector("header")
 const search = document.querySelector("input")
 const btn = document.querySelector("button")
+const cart = document.querySelector(".cart")
 
 
-async function getGit() {
-    btn.addEventListener("click", async function Get() {
-        const response = await fetch("https://api.github.com/users");
-        const jsonData = await response.json();
 
-        jsonData
-            .forEach((element) => {
-                console.log(element);
-                fetch(`${element.url}`)
-                    .then((resp) => resp.json())
-                    .then((data) => {
-                        container.innerHTML += `
+btn.addEventListener("click", async function Get() {
+
+    fetch(`https://api.github.com/users/${search.value}`)
+    .then(res => res.json())
+    .then(data => getGit(data))
+    // const response = await fetch(`https://api.github.com/users/${search.value}`);
+    // const jsonData = await response.json();
+    
+    // jsonData
+    // .forEach((element) => {
+    //     console.log(element);
+        
+    // });
+    search.value = ""
+})
+
+function getGit(data) {   
+    console.log(data);
+            container.innerHTML += `
      <div class="cart">
         <div class="top">
             <div class="logo">
                             
-                <img src=${element.avatar_url} alt="">
+                <img src=${data.avatar_url} alt="">
             </div>
-
             <div class="text">
-                <h2>The Octocat</h2>
+                <h2>${data.name}</h2>
                 <div class="login">
                 ${data.created_at}
                 </div>
@@ -33,7 +41,7 @@ async function getGit() {
                 
                 
             </div>
-                <a href="">@octocat</a>
+                <a href="${data.html_url}">@octocat</a>
                 <p>${data.bio}</p>
                 
                 <div class="main">
@@ -52,33 +60,27 @@ async function getGit() {
                 <div> ${data.following} </div>
                 </div>
         </div>
-
         <div class="nav">
             <div class="navication ">
+            <i class="fa-solid fa-location-dot"></i>
                 ${data.location}
             </div>
-
             <div class="twitt">
+            <i class="fa-brands fa-twitter"></i>
             ${data.caged}
             </div>
-
             <div class="github">
-            
+            <i class="fa-solid fa-link"></i>
+            ${data.blog}
             </div>
             
             <div class="git">
-            
+            <i class="fa-brands fa-github"></i>
+            <a href="${data.html_url}">@github</a>
             </div>
             
             </div>
             </div>`
-                    })
-            });
 
-    })
 
 }
-
-
-getGit()
-
